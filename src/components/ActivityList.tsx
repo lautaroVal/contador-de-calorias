d220b8686c899b75264e1379461864848b6bd8ba
lautaroVal <valenzuelalautaro@yahoo.com>
2025-07-11 10:12:02 -0300

    Agregando un Icono para Editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import type { Activity } from "../types"
 import { categories } from "../data/categories";
 import { useMemo } from "react";
+import { PencilSquareIcon } from '@heroicons/react/24/outline'
 
 type ActivityListProps = {
   activities: Activity[]
@@ -19,6 +20,7 @@ export default function ActivityList({activities}: ActivityListProps) {
 
     {activities.map(activity=> (
       <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
+
         <div className="space-y-2 relative">
           <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-400'}`}>
             {categoryName(+activity.category)}
@@ -30,6 +32,11 @@ export default function ActivityList({activities}: ActivityListProps) {
           </p>
         </div>
         
+        <div className="flex gap-5 items-center">
+          <button>
+            <PencilSquareIcon className="w-8 h-8 text-slate-800"/>
+          </button>
+        </div>
       </div>
     ))}
     </>
