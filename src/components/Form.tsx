54a3180849c8ce14defbf681cb12ffdab422cb4d
lautaroVal <valenzuelalautaro@yahoo.com>
2025-07-16 18:28:33 -0300

    Llenando el Formulario cuando editamos una actividad

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { v4 as uuidv4 } from 'uuid'
 import type { ChangeEvent, Dispatch, FormEvent } from "react"
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
-import type { ActivityActions } from "../reducers/activty-reducer"
+import type { ActivityActions, ActivityState } from "../reducers/activty-reducer"
 
 type FormProps = {
   dispatch: Dispatch<ActivityActions>
+  state: ActivityState
 }
 
 const InitialState : Activity = {
@@ -16,9 +17,16 @@ const InitialState : Activity = {
     calories: 0
   }
 
-export default function Form({dispatch} : FormProps) {
+export default function Form({dispatch, state} : FormProps) {
 
   const [activity, setActivity] = useState<Activity>(InitialState)
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+      setActivity(selectActivity)
+    }
+  },[state.activeId])
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ['category','calories'].includes(e.target.id)
