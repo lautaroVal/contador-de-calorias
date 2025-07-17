99df03d539755560add368e64bc9ebd6225c6aa8
lautaroVal <valenzuelalautaro@yahoo.com>
2025-07-16 21:09:35 -0300

    Almacenando las actividades en LocalStorage

--- src/reducers/activty-reducer.ts
+++ src/reducers/activty-reducer.ts
@@ -10,8 +10,13 @@ export type ActivityState = {
     activeId: Activity['id']  //(lookup type) en TypeScript
 }
 
+const localStorageActivities = () : Activity[] => {
+    const activities = localStorage.getItem('activities')
+    return  activities ? JSON.parse(activities) : []
+}
+
 export const initialState: ActivityState = {
-    activities: [],
+    activities: localStorageActivities(),
     activeId: ''
 }
 
