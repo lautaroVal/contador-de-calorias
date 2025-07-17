151df830a40606f1d2cdcb9b63fdc4a39b07b0f3
lautaroVal <valenzuelalautaro@yahoo.com>
2025-07-16 21:38:31 -0300

    Creando la acción para reiniciar la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react"
+import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activty-reducer"
 import ActivityList from "./components/ActivityList"
@@ -11,12 +11,21 @@ function App() {
     localStorage.setItem('activities', JSON.stringify(state.activities))
   },[state.activities])
   
+  const CanRestarApp = () => useMemo(() => state.activities.length, [state.activities])
+
   return (
     <>
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias</h1>
+        <button 
+        className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+        disabled={!CanRestarApp()}
+        onClick={() => dispatch({type: 'restart-app'})}
+        >
+          Reiniciar App
+        </button>
         </div>
       </header>
 
